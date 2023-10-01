e8d2a88662ef6506afa4ccab8509d7997b5d85df
puryabzp <puryabzp@gmail.com>
2023-10-01 16:07:32 +0330

    support log compaction

--- consumer.py
+++ consumer.py
@@ -1,18 +1,32 @@
+import json
 from confluent_kafka import Consumer, KafkaError
 
-
 class KafkaConsumer:
-    def __init__(self):
-        self.bootstrap_servers = 'localhost:9092'
-        self.group_id = 'my_consumer_group'
+    def __init__(self, config_file):
+        self.config = self.load_config(config_file)
+
+    def load_config(self, config_file):
+        try:
+            with open(config_file, 'r') as file:
+                config = json.load(file)
+            return config
+        except FileNotFoundError:
+            print(f"Config file '{config_file}' not found.")
+            exit(1)
+        except json.JSONDecodeError:
+            print(f"Invalid JSON format in config file '{config_file}'.")
+            exit(1)
 
     def consume(self):
         consumer = Consumer({
-            'bootstrap.servers': self.bootstrap_servers,
-            'group.id': self.group_id,
+            'bootstrap.servers': self.config.get('bootstrap_servers', 'localhost:9092'),
+            'group.id': self.config.get('group_id', 'my_consumer_group'),
             'auto.offset.reset': 'earliest'
         })
-        consumer.subscribe(['your-topics'])  # Add your desired topics here
+
+        # Read topics from the configuration file
+        topics = self.config.get('topics', ['inventory'])
+        consumer.subscribe(topics)
 
         while True:
             message = consumer.poll(1.0)
@@ -30,7 +44,6 @@ class KafkaConsumer:
     def run(self):
         self.consume()
 
-
 if __name__ == '__main__':
-    consumer = KafkaConsumer()
+    consumer = KafkaConsumer('config.json')  # Pass the config file path here
     consumer.run()
--- producer.py
+++ producer.py
@@ -1,28 +1,50 @@
+import json
 from confluent_kafka import Producer
 
 
 class KafkaProducer:
-    def __init__(self):
-        self.bootstrap_servers = 'localhost:9092'
+    def __init__(self, config_file):
+        self.config = self.load_config(config_file)
+
+    def load_config(self, config_file):
+        try:
+            with open(config_file, 'r') as file:
+                config = json.load(file)
+            return config
+        except FileNotFoundError:
+            print(f"Config file '{config_file}' not found.")
+            exit(1)
+        except json.JSONDecodeError:
+            print(f"Invalid JSON format in config file '{config_file}'.")
+            exit(1)
 
     def produce(self):
-        producer = Producer({'bootstrap.servers': self.bootstrap_servers})
+        producer = Producer({'bootstrap.servers': self.config.get('bootstrap_servers', 'localhost:9092')})
         while True:
             topic = input("Enter the topic (or 'exit' to quit): ")
             if topic == "exit":
                 break
-            message = input("Enter the message: ")
-            self.send_message(producer, topic, message)
+            key_value = input("Enter the key-value pair (e.g., '104,30') or just the value: ")
 
-    def send_message(self, producer, topic, message):
-        producer.produce(topic=topic, value=message)
+            if ',' in key_value:
+                key, value = key_value.split(',')  # Split the input into key and value
+                self.send_message(producer, topic, key, value)
+            else:
+                self.send_message(producer, topic, None, key_value)
+
+    def send_message(self, producer, topic, key, value):
+        message_key = key.encode('utf-8') if key else None
+        producer.produce(topic=topic, key=message_key, value=value.encode('utf-8'))
         producer.flush()
-        print(f"Sent message: {message}")
+        if key:
+            print(f"Sent message with key: {key}, value: {value}")
+        else:
+            print(f"Sent message: {value}")
 
     def run(self):
         self.produce()
 
 
 if __name__ == '__main__':
-    producer = KafkaProducer()
+    producer = KafkaProducer('config.json')  # Pass the config file path here
     producer.run()
